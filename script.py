335c6537d28c348a0719eb5a94c2c1963c40b1e4
truongpn <cs.truongpn@gmail.com>
2024-06-02 12:41:18 +0700

    update: change the format response

--- script.py
+++ script.py
@@ -1,21 +1,20 @@
+#pip install prettytable pandas
 import json
 import pandas as pd
 from itertools import groupby
 from operator import itemgetter
+from prettytable import PrettyTable
 
-df = pd.read_csv("data.csv", usecols=["Items","ImageURL","Categories","Ratio"])
+df = pd.read_csv("data.csv")
 df = df.to_dict(orient="records")
 
 for k, v in groupby(df, key=itemgetter('Categories')):
     v = list(v)
-    new_v = []
-    for i in v:
+    table = PrettyTable(["ID", "Item", "Ratio"])
+    for index, i in enumerate(v):
+        del i["ImageURL"]
         del i["Categories"]
-        new_v.append(i)
-    lenght = len(new_v)
-    with open(f"{k}.html", "w") as f:
-        response = {
-            "lenght": lenght,
-            "data": new_v
-        }
-        json.dump(response, f)
+        table.add_row([index+1, i["Items"], i["Ratio"]])
+
+    with open(f"{k}.html", "w", encoding="utf-8") as f:
+        f.write(str(table))

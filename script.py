c67301f62601a0bc8d98cf94ee5c3379534b5aa8
truongpn <cs.truongpn@gmail.com>
2024-06-02 12:06:15 +0700

    seperate categories

--- script.py
+++ script.py
@@ -6,7 +6,6 @@ from operator import itemgetter
 df = pd.read_csv("data.csv", usecols=["Items","ImageURL","Categories","Ratio"])
 df = df.to_dict(orient="records")
 
-grouped_data = {}
 for k, v in groupby(df, key=itemgetter('Categories')):
     v = list(v)
     new_v = []
@@ -14,18 +13,9 @@ for k, v in groupby(df, key=itemgetter('Categories')):
         del i["Categories"]
         new_v.append(i)
     lenght = len(new_v)
-    grouped_data[k] = {
-        "lenght": lenght,
-        "data": new_v
-    }
-
-
-response = {
-    "Sword": grouped_data["Sword"],
-    "Accessory": grouped_data["Accessory"],
-    "Material": grouped_data["Material"],
-    "Stone": grouped_data["Stone"],
-    "GamePass": grouped_data["GamePass"],
-}
-with open("index.html", "w") as f:
-    json.dump(response, f)
+    with open(f"{k}.html", "w") as f:
+        response = {
+            "lenght": lenght,
+            "data": new_v
+        }
+        json.dump(response, f)
